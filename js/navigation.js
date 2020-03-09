2934385c855ecf4531d249ea6c922790447d4031
Nejc_Gorsic <nejcgorsic1@gmail.com>
2020-03-09 22:41:33 +0100

    Mobile / Desktop

--- js/navigation.js
+++ js/navigation.js
@@ -1,7 +1,13 @@
-$('.nav-item').hover(function(){
-    $(this).closest('li').find('.navbar-nav__sub').css({'opacity':0,'margin-top':15}).show().animate({'margin-top':0,'opacity':1},300);
-},function(){
-    $(this).closest('li').find('.navbar-nav__sub').fadeOut(200,function(){
-         $(this).hide();   
+$('.nav-item').hover(function () {
+    $(this).closest('li').find('.navbar-nav__sub').css({
+        'opacity': 0,
+        'margin-top': 15
+    }).show().animate({
+        'margin-top': 0,
+        'opacity': 1
+    }, 300);
+}, function () {
+    $(this).closest('li').find('.navbar-nav__sub').fadeOut(200, function () {
+        $(this).hide();
     });
 });
