db395631af9280ab33df039587992e7f104c14bf
Nejc Goršič <nejcgorsic1@gmail.com>
2020-03-10 18:24:53 +0100

    Beeutified the links

--- js/navigation.js
+++ js/navigation.js
@@ -1,11 +1,12 @@
 $('.nav-item').hover(function () {
     $(this).closest('li').find('.navbar-nav__sub').css({
         'opacity': 0,
-        'margin-top': 15
+        'margin-top': -10
     }).show().animate({
         'margin-top': 0,
         'opacity': 1
     }, 300);
+
 }, function () {
     $(this).closest('li').find('.navbar-nav__sub').fadeOut(200, function () {
         $(this).hide();
